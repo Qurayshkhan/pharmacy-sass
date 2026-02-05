d513c5ce0085d45ddc943ed99230271f5532d44b
Hassan khan <hk147471@gmail.com>
2026-02-06 01:02:36 +0500

    fix: error  'router' is defined but never used  @typescript-eslint/no-unused-vars

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link, router } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 import { BookOpen, Folder, LayoutGrid, Store } from 'lucide-react';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
