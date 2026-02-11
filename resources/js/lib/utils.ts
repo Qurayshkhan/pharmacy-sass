3dca61f8be9b169b0ea9e588fac24a40bcf81055
Hassan khan <hk147471@gmail.com>
2026-02-11 18:51:32 +0500

    feat: search medicine

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,6 @@
 import type { InertiaLinkProps } from '@inertiajs/react';
-import React from 'react';
 import { type ClassValue, clsx } from 'clsx';
+import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
